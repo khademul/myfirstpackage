7b3f9fffe151137b60a5be82a12050fb64b12a3b
khademul <khademul144@gmail.com>
2017-08-25 16:20:52 -0700

    several changes

---
+++ R/read_data.R
@@ -0,0 +1,10 @@
+#' read data in 'file_name' as a data frame
+#'
+#' @param file_name name of the file that contains bike counts
+#' @return a data frame with bike counts
+#' @export
+read_data <- function(file_name) {
+  print(paste(file_name, "is ready to be read into R"))
+  my_df <- readxl::read_excel(file_name)
+  return(my_df)
+}
